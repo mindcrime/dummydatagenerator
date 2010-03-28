31578a4e9593ab0f1a8b3d879cf3e3b6d97561c1
Phillip Rhodes <mindcrime@cpphacker.co.uk>
2010-03-27 20:55:38 -0400

    fixed run.sh to support new command line args

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ loc_jars=$(echo ./lib/*.jar | sed 's/ /:/g')':'
 
 # echo $loc_jars
 
-java -cp $loc_jars datagen.DataGenMain $1 $2 $3 $4 $5
+java -cp $loc_jars datagen.DataGenMain $1 $2 $3 $4 $5 $6 $7
